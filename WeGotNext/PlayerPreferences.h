0544d20bbeb80eb55a15a68dc2bb765777196396
nzayatz14 <nzayatz14@hotmail.com>
2014-09-01 18:15:30 -0400

    added class reference to PlayerPreferences Window.

---
+++ WeGotNext/PlayerPreferences.h
@@ -0,0 +1,13 @@
+//
+//  PlayerPreferences.h
+//  WeGotNext
+//
+//  Created by Nick Zayatz on 9/1/14.
+//  Copyright (c) 2014 Nick Zayatz and Charlie Adams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayerPreferences : UIViewController
+
+@end
---
+++ WeGotNext/PlayerPreferences.m
@@ -0,0 +1,13 @@
+//
+//  PlayerPreferences.m
+//  WeGotNext
+//
+//  Created by Nick Zayatz on 9/1/14.
+//  Copyright (c) 2014 Nick Zayatz and Charlie Adams. All rights reserved.
+//
+
+#import "PlayerPreferences.h"
+
+@implementation PlayerPreferences
+
+@end
